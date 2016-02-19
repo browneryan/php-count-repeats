8607331030a364e0edce13b410ea5b3296944e31
Ryan Brown <browneryan87@gmail.com>
2016-02-19 09:55:15 -0800

    Add second passing spec

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,9 +1,12 @@
 <?php
     class RepeatCounter
     {
-        function countRepeats($user_sentence)
+        function countRepeats($user_sentence, $user_word)
         {
-            return $user_sentence;
+            $user_array = array();
+            array_push($user_array, $user_sentence);
+            array_push($user_array, $user_word);
+            return $user_array;
         }
     }
 ?>
--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -4,14 +4,15 @@
 
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
-        function test_countRepeats()
+        function test_if_user_input_in_countRepeats()
         {
             $test_count_repeats = new RepeatCounter;
+            $user_word = "word";
             $user_sentence = "This is a sentence.";
 
-            $result = $test_count_repeats->countRepeats($user_sentence);
+            $result = $test_count_repeats->countRepeats($user_sentence, $user_word);
 
-            $this->assertEquals("This is a sentence.", $result);
+            $this->assertEquals(["This is a sentence.", "word"], $result);
         }
     }
 
