5f149da1105ca652ed5ea2cf9abef5f7aa6fc97e
Ryan Brown <browneryan87@gmail.com>
2016-02-19 10:35:56 -0800

    Add punctuation spec to RepeatCounterTest.php

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -4,7 +4,7 @@
 
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
-        function test_sentence_explode()
+        function test_full_word_match()
         {
             $test_count_repeats = new RepeatCounter;
             $user_sentence = "Looking for word";
@@ -13,6 +13,26 @@
             $result = $test_count_repeats->countRepeats($user_sentence, $user_word);
 
             $this->assertEquals(1, $result);
+        }
+        function test_partial_word_no_match()
+        {
+            $test_count_repeats = new RepeatCounter;
+            $user_sentence = "Not words but word";
+            $user_word = "word";
+
+            $result = $test_count_repeats->countRepeats($user_sentence, $user_word);
+
+            $this->assertEquals(1, $result);
+        }
+        function test_punctuation()
+        {
+            $test_count_repeats = new RepeatCounter;
+            $user_sentence = "Looking for word, not words, but word.";
+            $user_word = "word";
+
+            $result = $test_count_repeats->countRepeats($user_sentence, $user_word);
+
+            $this->assertEquals(2, $result);
         }
     }
 
