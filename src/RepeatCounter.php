77b37e0a1261c85174e7d9638043eeff4ba21451
Ryan Brown <browneryan87@gmail.com>
2016-02-19 10:13:10 -0800

    Add passing new first spec.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,12 +1,10 @@
 <?php
     class RepeatCounter
     {
-        function countRepeats($user_sentence, $user_word)
+        function countRepeats($user_sentence)
         {
-            $user_array = array();
-            array_push($user_array, $user_sentence);
-            array_push($user_array, $user_word);
-            return $user_array;
+            $split_sentence = explode(" ", $user_sentence);
+            return $split_sentence;
         }
     }
 ?>
--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -4,24 +4,14 @@
 
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
-        function test_if_user_input_in_countRepeats()
-        {
-            $test_count_repeats = new RepeatCounter;
-            $user_word = "word";
-            $user_sentence = "This is a sentence";
-
-            $result = $test_count_repeats->countRepeats($user_sentence, $user_word);
-
-            $this->assertEquals(["This is a sentence", "word"], $result);
-        }
         function test_sentence_explode()
         {
             $test_count_repeats = new RepeatCounter;
             $user_sentence = "This is a sentence";
 
-            $result = $test_count_repeats->countRepeats($user_sentence, $user_word);
+            $result = $test_count_repeats->countRepeats($user_sentence);
 
-            $this->assertEquals(["This", "is", "a", "sentence", "word"], $result);
+            $this->assertEquals(["This", "is", "a", "sentence"], $result);
         }
     }
 
