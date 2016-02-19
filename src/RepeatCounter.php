d702e394445b7ed8d7dc796b737e1f5328c855e6
Ryan Brown <browneryan87@gmail.com>
2016-02-19 12:14:35 -0800

    Add srttolower for user word search input in RepeatCounter.php

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,12 +4,14 @@
         function countRepeats($user_sentence, $user_word)
         {
             $lowercase_sentence = strtolower($user_sentence);
+            $lowercase_word = strtolower($user_word);
+
             $split_sentence = preg_split("/[\W]+/", $lowercase_sentence);
 
             $count = 0;
 
             foreach ($split_sentence as $word) {
-                if ($user_word == $word) {
+                if ($lowercase_word == $word) {
                 $count++;
                 }
             }
