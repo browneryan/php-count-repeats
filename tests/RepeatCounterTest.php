a5d7569fe609bea556f7eb43086442050da058ba
Ryan Brown <browneryan87@gmail.com>
2016-02-19 09:15:14 -0800

    Add basic RepeatCounterTest.php structure

---
+++ tests/RepeatCounterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+    require_once "src/CountRepeats.php";
+
+    class RepeatCounterTest extends PHPUnit_Framework_TestCase
+    {
+        function test_countRepeats()
+        {
+
+        }
+    }
+
+?>
